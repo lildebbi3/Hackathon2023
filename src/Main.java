24f92664e21157080923cc8965add27b8b2fd48a
hill <hillhouse.kyle@gmail.com>
2023-02-24 14:15:14 -0500

    Menu WIP2

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,44 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-       // readDataTest();
-        List<String[]> rows = CsvReader.readCsv();
-        rows = DataGrabber.StateGrabber(rows, "GA");
-        rows = DataGrabber.IndustryGrabber(rows, " Electrical contractors ");
+        Scanner scanner = new Scanner(System.in);
+        String state;
+        String industry;
+        boolean exit = false;
 
-        List<String[]> filteredRows = new ArrayList<>();
+        while (!exit) {
+            System.out.println("Please choose an option:");
+            System.out.println("1. Enter state");
+            System.out.println("2. Enter industry");
+            System.out.println("3. Search");
+            System.out.println("4. Exit program");
 
-        for (String[] row : rows) {
-            if (row[6].equals("GA") && row[9].equals(" Electrical contractors ")) {
-                filteredRows.add(row);
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // consume the newline character
+
+            switch (choice) {
+                case 1 -> {
+                    System.out.print("Enter state (Ex: GA, NC): ");
+                    state = scanner.nextLine();
+                    System.out.println("You entered: " + state);
+                }
+                case 2 -> {
+                    System.out.print("Enter industry (Ex: Construction, healthcare): ");
+                    industry = scanner.nextLine();
+                    System.out.println("You entered: " + industry);
+                }
+                case 3 -> System.out.println("Executing search...");
+
+                // execute the search based on the user's state and industry inputs
+                // TODO: implement search logic here
+                case 4 -> {
+                    System.out.println("Exiting program...");
+                    exit = true;
+                }
+                default -> System.out.println("Invalid choice. Please try again.");
             }
         }
 
-        readDataTest(filteredRows);
-    }
-
-    //Method to that tests out the CsvReader.readCsv class/method that obtains the data and prints
-    //them out in the console. 1st loop goes through 20 rows,2nd loop through corresponding row's 28 columns
-    public static void readDataTest(List<String[]> rows) {
-        int numRows = Math.min(rows.size(), 20);
-        for (int i = 0; i < numRows; i++) {
-            String[] arr = rows.get(i);
-            for (int e = 0; e < arr.length; e++) {
-                System.out.print(arr[e] + " ");
-            }
-            System.out.println();
-        }
+        scanner.close();
     }
 }
