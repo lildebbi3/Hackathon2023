7a87804211f47a01ba83d195f96c06395f6eea3f
c-floressantos <114621197+c-floressantos@users.noreply.github.com>
2023-02-24 13:58:25 -0500

    DataGrabber.StateGrabber works correctly

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -5,19 +6,29 @@ public class Main {
        // readDataTest();
         List<String[]> rows = CsvReader.readCsv();
         rows = DataGrabber.StateGrabber(rows, "GA");
-        readDataTest(rows);
+        rows = DataGrabber.IndustryGrabber(rows, " Electrical contractors ");
+
+        List<String[]> filteredRows = new ArrayList<>();
+
+        for (String[] row : rows) {
+            if (row[6].equals("GA") && row[9].equals(" Electrical contractors ")) {
+                filteredRows.add(row);
+            }
+        }
+
+        readDataTest(filteredRows);
     }
 
     //Method to that tests out the CsvReader.readCsv class/method that obtains the data and prints
     //them out in the console. 1st loop goes through 20 rows,2nd loop through corresponding row's 28 columns
-    public static void readDataTest(List<String[]> rows ){
-        for(int i=0;i<20;i++){
+    public static void readDataTest(List<String[]> rows) {
+        int numRows = Math.min(rows.size(), 20);
+        for (int i = 0; i < numRows; i++) {
             String[] arr = rows.get(i);
-            for(int e=0;e<28;e++){
-                System.out.print(arr[e]+" ");
+            for (int e = 0; e < arr.length; e++) {
+                System.out.print(arr[e] + " ");
             }
             System.out.println();
         }
     }
-
 }
