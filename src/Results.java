6adb81ff710f79eb945cd300b624893455a42ae5
Merrick McPherson <merrickscottmcpherson@gmail.com>
2023-02-24 10:38:32 -0500

    Result class created, it holds the state, industry, and an int[] injuries. with corresponding methods.

---
+++ src/Results.java
@@ -0,0 +1,38 @@
+public class Results {
+    private String state;
+    private String industry;
+    private int[] injuries = new int[7];
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public int[] getInjuries() {
+        return injuries;
+    }
+
+    public void setInjuries(int[] injuries) {
+        this.injuries = injuries;
+    }
+
+    public void print() {
+        System.out.println("State: " + state);
+        System.out.println("Industry: " + industry);
+        System.out.println("Injuries:");
+        for (int i = 0; i < injuries.length; i++) {
+            System.out.println("Week " + (i+1) + ": " + injuries[i]);
+        }
+    }
+}
